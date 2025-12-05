6706f932b5f3c18b8f72603fb4256e4235bea1e6
dongE <wlsemrdl1212@naver.com>
2025-12-05 23:37:25 +0900

    inital commit

---
+++ fcm_be/src/main/java/com/example/demo/FcMdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcMdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcMdemoApplication.class, args);
+	}
+
+}
